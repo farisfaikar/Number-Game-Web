64e932b3d8e5053c52343a84162c81d54d9bace8
farisfaikar <farisfaikar.r@gmail.com>
2023-09-24 20:49:24 +0700

    feat: make terminal javascript generated, also adjust prettier

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 // ===== Global Variables =====
 // Elements
 let display = document.getElementById("display");
-let terminalTitle = document.getElementById("terminal-title");
+// let terminalTitle = document.getElementById("terminal-title");
 let terminal = document.getElementById("terminal");
 let xKey = document.getElementById("k-x");
 let equalsKey = document.getElementById("k-=");
@@ -11,22 +11,24 @@ let achievementKey = document.getElementById("k-ac");
 
 // Variables
 let listNumKeys = ["k-1", "k-2", "k-3", "k-4", "k-5", "k-6", "k-7", "k-8", "k-9", "k-0"];
+let gameState = "start";
 let inputNumber = "";
+let secretNumber = "";
 let count = 1;
 let correctNumbers = 0;
 let correctPositions = 0;
-let gameState = "start";
 let attempts = 0;
-let maxAttempts = 8;
 let remainingAttempts = 0;
-let secretNumber = "";
 let isWinTextDisplayed = false;
+
+// Constants
+const MAX_ATTEMPTS = 8;
 
 // ===== Listeners =====
 function numKeyListeners() {
   for (let i = 0; i < listNumKeys.length; i++) {
     let numKey = document.getElementById(listNumKeys[i]);
-    numKey.addEventListener('click', () => {
+    numKey.addEventListener("click", () => {
       if (inputNumber.length < 4) {
         let num = numKey.getAttribute("after");
         inputNumber += num;
@@ -40,7 +42,7 @@ function numKeyListeners() {
 }
 
 function deleteKeyListeners() {
-  xKey.addEventListener('click', () => {
+  xKey.addEventListener("click", () => {
     resetDisplay();
   });
 }
@@ -48,6 +50,7 @@ function deleteKeyListeners() {
 function resetDisplay() {
   inputNumber = "";
   display.innerHTML = "";
+  // Reset disabled keys
   for (let i = 0; i < listNumKeys.length; i++) {
     let numKey = document.getElementById(listNumKeys[i]);
     numKey.removeAttribute("disabled");
@@ -57,7 +60,7 @@ function resetDisplay() {
 }
 
 function submitKeyListeners() {
-  equalsKey.addEventListener('click', () => {
+  equalsKey.addEventListener("click", () => {
     compare();
     if (inputNumber.length === 4 && gameState === "start") {
       let newParagraph = document.createElement("p");
@@ -73,31 +76,47 @@ function submitKeyListeners() {
 }
 
 function restartKeyListeners() {
-  restartKey.addEventListener('click', () => {
+  restartKey.addEventListener("click", () => {
     // Remove all child elements of the terminal div except the first one
     while (terminal.lastChild && terminal.childElementCount > 1) {
       terminal.removeChild(terminal.lastChild);
-    };
+    }
     count = 1;
     isWinTextDisplayed = false;
+    remainingAttempts = MAX_ATTEMPTS;
+    attempts = 0;
     createSecretNumber();
     resetDisplay();
+    removeOldTerminalTitle();
+    init();
   });
 }
 
 function highscoreKeyListeners() {
-  highschoreKey.addEventListener('click', () => {
+  highschoreKey.addEventListener("click", () => {
     window.location.href = "highscore.html";
   });
 }
 
 function achievementKeyListeners() {
-  achievementKey.addEventListener('click', () => {
+  achievementKey.addEventListener("click", () => {
     window.location.href = "achievement.html";
   });
 }
 
 // ===== Game Logic =====
+function init() {
+  let terminalTitle = document.createElement("p");
+  terminalTitle.id = "terminal-title";
+  terminalTitle.innerHTML = `Guess the 4 digit number combination! You have ${MAX_ATTEMPTS} attempts left.`;
+  terminal.appendChild(terminalTitle);
+}
+
+function removeOldTerminalTitle() {
+  let oldTerminalTitle = document.getElementById("terminal-title");
+  oldTerminalTitle.remove();
+}
+
 function createSecretNumber() {
   secretNumber = "";
   while (secretNumber.length < 4) {
@@ -127,10 +146,11 @@ function compare() {
       }
     }
     attempts += 1;
-    remainingAttempts = maxAttempts - attempts;
-    if (attempts >= maxAttempts) {
+    remainingAttempts = MAX_ATTEMPTS - attempts;
+    if (attempts >= MAX_ATTEMPTS) {
       gameState = "lost";
     }
+    let terminalTitle = document.getElementById("terminal-title");
     terminalTitle.innerHTML = `Guess the 7 digit number combination! You have ${remainingAttempts} attempts left.`;
   }
 }
@@ -151,6 +171,7 @@ function displayWinText() {
 // ===== Main Function =====
 function main() {
   // Init
+  init();
   createSecretNumber();
   // Listeners
   numKeyListeners();
